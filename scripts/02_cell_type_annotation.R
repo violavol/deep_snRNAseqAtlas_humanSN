a9f68614dcab86eeaab01e312429c16be32bf334
violavol <33480610+violavol@users.noreply.github.com>
2026-03-27 10:46:42 +0000

    Update 02_cell_type_annotation.R

--- scripts/02_cell_type_annotation.R
+++ scripts/02_cell_type_annotation.R
@@ -5,14 +5,20 @@ library(here)
 load(here("data/processed/sn_atlas_integrated.RData"))
 
 # Cell type annotation
-DefaultAssay(combined_all) <- "RNA"
+DefaultAssay(sn_combined) <- "RNA"
 
 markersALL<-c("GFRA2","CALCR","CRYM","CCDC68","PPP1R17","TRHR","TH","GRIN2C","SLC18A2","DCX","LMX1A","GAD2","GAD1","NTSR1","SLC6A3","KCNJ6","PITX3","CHRNA4","LMX1B","GRIK3","ALDH1A1","AQP4","GFAP","VCAN","MOBP","MOG","CSF1R","CD8A","PTPRC","CLDN5","PTH1R")
 
+DotPlot(sn_combined, features = markersALL, dot.scale = 8) +
+    RotatedAxis()
 
-for (cell_type in names(markers)) {
-  sn_combined <- AddModuleScore(sn_combined, features = list(markers[[cell_type]]), name = cell_type)
-}
+sn_combined$cluster <- Idents(sn_combined)
+cellType <- c("ODC","ODC","DaN","DaN","Microglia","Astrocyte","OPC","GABA","DaN","DaN","DaN","Tcell")
+names(cellType) <- levels(sn_combined)
+sn_combined <- RenameIdents(sn_combined, cellType)
+sn_combined$CellType<-Idents(sn_combined)
+
+DimPlot(sn_combined, reduction = "umap", split.by = "Disease",group.by="CellType")
 
 # Save annotated object
 save(sn_combined, file = here("data/processed/sn_atlas_annotated.RData"))
