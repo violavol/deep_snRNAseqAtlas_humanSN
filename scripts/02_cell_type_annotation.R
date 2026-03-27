c62e3a0371b28c492065c0eeb02be4c2c3717658
violavol <33480610+violavol@users.noreply.github.com>
2026-03-27 10:45:05 +0000

    Update 02_cell_type_annotation.R

--- scripts/02_cell_type_annotation.R
+++ scripts/02_cell_type_annotation.R
@@ -5,12 +5,10 @@ library(here)
 load(here("data/processed/sn_atlas_integrated.RData"))
 
 # Cell type annotation
-# Example: based on canonical markers
-markers <- list(
-  Dopaminergic = c("TH","SLC6A3"),
-  Astrocytes = c("GFAP","AQP4"),
-  Microglia = c("CX3CR1","P2RY12")
-)
+DefaultAssay(combined_all) <- "RNA"
+
+markersALL<-c("GFRA2","CALCR","CRYM","CCDC68","PPP1R17","TRHR","TH","GRIN2C","SLC18A2","DCX","LMX1A","GAD2","GAD1","NTSR1","SLC6A3","KCNJ6","PITX3","CHRNA4","LMX1B","GRIK3","ALDH1A1","AQP4","GFAP","VCAN","MOBP","MOG","CSF1R","CD8A","PTPRC","CLDN5","PTH1R")
+
 
 for (cell_type in names(markers)) {
   sn_combined <- AddModuleScore(sn_combined, features = list(markers[[cell_type]]), name = cell_type)
