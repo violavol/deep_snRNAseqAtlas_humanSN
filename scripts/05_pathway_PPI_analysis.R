67fbe250bb2dff629021c765b0a8dc5c2ffac5b4
violavol <33480610+violavol@users.noreply.github.com>
2026-04-13 15:42:57 +0100

    Update 05_pathway_PPI_analysis.R

--- scripts/05_pathway_PPI_analysis.R
+++ scripts/05_pathway_PPI_analysis.R
@@ -5,7 +5,7 @@ library(igraph)
 library(here)
 
 # Load DEGs
-load(here("results/DEG_tables/DA_DEG_results.RData"))
+load(here("results/DEG_tables/DEG_results.RData"))
 
 # Function to perform GO enrichment for a list of genes
 go_enrichment <- function(gene_list, ont = "BP") {
@@ -19,23 +19,36 @@ go_enrichment <- function(gene_list, ont = "BP") {
 }
 
 # Loop over each cluster's DEGs
-go_results <- list()
+go_results_degs <- list()
 for (clust in names(deg_results)) {
   genes <- rownames(deg_results[[clust]])[deg_results[[clust]]$p_val_adj < 0.05]
   if(length(genes) > 0) {
-    go_results[[clust]] <- go_enrichment(genes)
+    go_results_degs[[clust]] <- go_enrichment(genes)
   }
 }
 
-# Optional: construct PPI network for top DEGs using STRINGdb (or igraph)
-# Here we create a simple example with igraph
-ppi_edges <- data.frame(from=c(), to=c())
-for (clust in names(go_results)) {
-  top_genes <- head(rownames(deg_results[[clust]]), 20)
-  edges <- expand.grid(top_genes, top_genes)
-  edges <- edges[edges$Var1 != edges$Var2, ]
-  ppi_edges <- rbind(ppi_edges, edges)
+
+
+# Cell type-specific PPI networks
+# to be done for DaN_3, ODC_2 and OPC (where PD genetic risk converged)
+
+ppi_net<-read.delim("ppi_net_new2023short",h=F)
+ppi_net_dan3top1000<-ppi_net[ppi_net[,1]%in%rownames(df_w_test_DaN[oder(-df_w_test_DaN$DaN_3),])[1:1000] & ppi_net[,2]%in%rownames(df_w_test_DaN[oder(-df_w_test_DaN$DaN_3),])[1:1000] ,]
+ppi_net_dan3top1000_genes<-unique(c(as.character(ppi_net_dan3top1000[,1]),as.character(ppi_net_dan3top1000[,2])))
+
+net_1 <- graph_from_data_frame(d= ppi_net_dan3top1000, vertices= ppi_net_dan3top1000_genes, directed=F)
+cl_net_1<-cluster_louvain(net_1)
+memb_net_1<-as.matrix(membership(cl_net_1))
+memb_net_dan3top1000ppi_df<-data.frame(id=rownames(memb_net_1),module=memb_net_1)
+ppi_results <- memb_net_dan3top1000ppi_df
+
+# Loop over each gene module
+go_results_ppi_network <- list()
+for (module in unique(ppi_results$module)) {
+  genes <- ppi_results[ppi_results$module==module,1]
+  if(length(genes) > 0) {
+    go_results_ppi_network[[module]] <- go_enrichment(genes)
+  }
 }
 
-ppi_network <- graph_from_data_frame(ppi_edges, directed = FALSE)
-save(go_results, ppi_network, file = here("results/GO_PPI/GO_PPI_results.RData"))
+save(go_results_degs, go_results_ppi_network, file = here("results/GO_PPI/GO_PPI_results.RData"))
